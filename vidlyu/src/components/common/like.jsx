4b841d1f5fea342656f34f7964315ed940979c19
zalogarciam <gegarciam95@gmail.com>
2022-12-06 12:04:50 -0500

    Remove this

--- vidlyu/src/components/common/like.jsx
+++ vidlyu/src/components/common/like.jsx
@@ -3,10 +3,10 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeart as solidHeart } from "@fortawesome/free-solid-svg-icons";
 import { faHeart as lightHeart } from "@fortawesome/free-regular-svg-icons";
 
-const Like = () => {
-    if (this.props.liked)
-     return <FontAwesomeIcon onClick={this.props.onClick} icon={solidHeart} style={{cursor: 'pointer'}} />;
-    else return <FontAwesomeIcon onClick={this.props.onClick} icon={lightHeart} style={{cursor: 'pointer'}}/>; 
+const Like = (props) => {
+    if (props.liked)
+     return <FontAwesomeIcon onClick={props.onClick} icon={solidHeart} style={{cursor: 'pointer'}} />;
+    else return <FontAwesomeIcon onClick={props.onClick} icon={lightHeart} style={{cursor: 'pointer'}}/>; 
 }
  
 export default Like;
