44a4a71223b4e478dce9e1b4c8656d0101c1fbbd
zalogarciam <gegarciam95@gmail.com>
2022-12-29 15:40:10 -0500

    Handle when there is only one page

--- vidlyu/src/components/common/pagination.jsx
+++ vidlyu/src/components/common/pagination.jsx
@@ -1,29 +1,25 @@
 import React, { Component } from "react";
+import _ from "lodash";
 
-class Pagination extends Component {
-  render() {
-    return (
-      <nav aria-label="Page navigation example">
-        <ul className="pagination">
-          <li className="page-item">
+const Pagination = (props) => {
+  const { itemsCount, pageSize } = props;
+
+  const pagesCount = Math.ceil(itemsCount / pageSize);
+  if (pagesCount === 1) return null;
+  const pages = _.range(1, pagesCount + 1);
+  return (
+    <nav aria-label="Page navigation example">
+      <ul className="pagination">
+        {pages.map((page) => (
+          <li key={page} className="page-item">
             <a className="page-link" href="#">
-              1
+              {page}
             </a>
           </li>
-          <li className="page-item">
-            <a className="page-link" href="#">
-              2
-            </a>
-          </li>
-          <li className="page-item">
-            <a className="page-link" href="#">
-              3
-            </a>
-          </li>
-        </ul>
-      </nav>
-    );
-  }
-}
+        ))}
+      </ul>
+    </nav>
+  );
+};
 
 export default Pagination;
