9b283e8cc5bb002134379816cee53ce43cd3ecc2
zalogarciam <gegarciam95@gmail.com>
2023-01-05 17:39:01 -0500

    Adding row - cols to separate list group with table

--- vidlyu/src/components/movie.jsx
+++ vidlyu/src/components/movie.jsx
@@ -37,51 +37,57 @@ class Movie extends Component {
     const movies = paginate(allMovies, currentPage, pageSize);
 
     return (
-      <div className="body">
-        <ListGroup></ListGroup>
-        <p> Showing {count} movies in the database</p>
-        <table className="table">
-          <thead>
-            <tr>
-              <th scope="col">Title</th>
-              <th scope="col">Genre</th>
-              <th scope="col">Number In Stock</th>
-              <th scope="col">Daily Rental Rate</th>
-              <th scope="col">Like</th>
-              <th scope="col">Option</th>
-            </tr>
-          </thead>
-          <tbody>
-            {movies.map((movie) => (
-              <tr key={movie._id}>
-                <td>{movie.title}</td>
-                <td>{movie.genre.name}</td>
-                <td>{movie.numberInStock}</td>
-                <td>{movie.dailyRentalRate}</td>
-                <td>
-                  <Like
-                    liked={movie.liked}
-                    onClick={() => this.handleLike(movie)}
-                  ></Like>
-                </td>
-                <td>
-                  <button
-                    className="btn btn-danger"
-                    onClick={() => this.handleDelete(movie)}
-                  >
-                    Delete
-                  </button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-        <Pagination
-          itemsCount={count}
-          pageSize={pageSize}
-          currentPage={currentPage}
-          onPageChange={this.handlePageChange}
-        ></Pagination>
+      <div className="body container">
+        <div className="row">
+          <div className="col-3">
+            <ListGroup></ListGroup>
+          </div>
+          <div className="col-9">
+            <p> Showing {count} movies in the database</p>
+            <table className="table">
+              <thead>
+                <tr>
+                  <th scope="col">Title</th>
+                  <th scope="col">Genre</th>
+                  <th scope="col">Number In Stock</th>
+                  <th scope="col">Daily Rental Rate</th>
+                  <th scope="col">Like</th>
+                  <th scope="col">Option</th>
+                </tr>
+              </thead>
+              <tbody>
+                {movies.map((movie) => (
+                  <tr key={movie._id}>
+                    <td>{movie.title}</td>
+                    <td>{movie.genre.name}</td>
+                    <td>{movie.numberInStock}</td>
+                    <td>{movie.dailyRentalRate}</td>
+                    <td>
+                      <Like
+                        liked={movie.liked}
+                        onClick={() => this.handleLike(movie)}
+                      ></Like>
+                    </td>
+                    <td>
+                      <button
+                        className="btn btn-danger"
+                        onClick={() => this.handleDelete(movie)}
+                      >
+                        Delete
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+            <Pagination
+              itemsCount={count}
+              pageSize={pageSize}
+              currentPage={currentPage}
+              onPageChange={this.handlePageChange}
+            ></Pagination>
+          </div>
+        </div>
       </div>
     );
   }
