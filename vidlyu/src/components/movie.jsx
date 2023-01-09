6c96bdaa0e18e3eb07494179820bf12158d02393
zalogarciam <gegarciam95@gmail.com>
2023-01-09 16:36:53 -0500

    Refactor and create Movies table component

--- vidlyu/src/components/movie.jsx
+++ vidlyu/src/components/movie.jsx
@@ -1,11 +1,11 @@
 import React, { Component, Fragment } from "react";
 import { getMovies, deleteMovie } from "../services/fakeMovieService";
 import { getGenres } from "../services/fakeGenreService";
-import Like from "./common/like";
 import Pagination from "./common/pagination";
 import { paginate } from "../utils/paginate";
 import ListGroup from "./common/listGroup";
 import { filter } from "lodash";
+import MoviesTable from "./moviesTable";
 
 class Movie extends Component {
   state = {
@@ -76,42 +76,11 @@ class Movie extends Component {
           </div>
           <div className="col-9">
             <p> Showing {filtered.length} movies in the database</p>
-            <table className="table">
-              <thead>
-                <tr>
-                  <th scope="col">Title</th>
-                  <th scope="col">Genre</th>
-                  <th scope="col">Number In Stock</th>
-                  <th scope="col">Daily Rental Rate</th>
-                  <th scope="col">Like</th>
-                  <th scope="col">Option</th>
-                </tr>
-              </thead>
-              <tbody>
-                {movies.map((movie) => (
-                  <tr key={movie._id}>
-                    <td>{movie.title}</td>
-                    <td>{movie.genre.name}</td>
-                    <td>{movie.numberInStock}</td>
-                    <td>{movie.dailyRentalRate}</td>
-                    <td>
-                      <Like
-                        liked={movie.liked}
-                        onClick={() => this.handleLike(movie)}
-                      ></Like>
-                    </td>
-                    <td>
-                      <button
-                        className="btn btn-danger"
-                        onClick={() => this.handleDelete(movie)}
-                      >
-                        Delete
-                      </button>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
+            <MoviesTable
+              movies={movies}
+              onLike={this.handleLike}
+              onDelete={this.handleDelete}
+            ></MoviesTable>
             <Pagination
               itemsCount={filtered.length}
               pageSize={pageSize}
---
+++ vidlyu/src/components/moviesTable.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+import Like from "./common/like";
+
+const MoviesTable = (props) => {
+  const { movies, onDelete, onLike } = props;
+  return (
+    <table className="table">
+      <thead>
+        <tr>
+          <th scope="col">Title</th>
+          <th scope="col">Genre</th>
+          <th scope="col">Number In Stock</th>
+          <th scope="col">Daily Rental Rate</th>
+          <th scope="col">Like</th>
+          <th scope="col">Option</th>
+        </tr>
+      </thead>
+      <tbody>
+        {movies.map((movie) => (
+          <tr key={movie._id}>
+            <td>{movie.title}</td>
+            <td>{movie.genre.name}</td>
+            <td>{movie.numberInStock}</td>
+            <td>{movie.dailyRentalRate}</td>
+            <td>
+              <Like liked={movie.liked} onClick={() => onLike(movie)}></Like>
+            </td>
+            <td>
+              <button
+                className="btn btn-danger"
+                onClick={() => onDelete(movie)}
+              >
+                Delete
+              </button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default MoviesTable;
