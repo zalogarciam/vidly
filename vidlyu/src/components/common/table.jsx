761934295e0caee349e6a451b850cb122c2cadd3
zalogarciam <gegarciam95@gmail.com>
2023-01-12 18:36:40 -0500

    Extract table component

---
+++ vidlyu/src/components/common/table.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import TableHeader from "./tableHeader";
+import TableBody from "./tableBody";
+
+const Table = (props) => {
+  const { columns, sortColumn, onSort, data } = props;
+  return (
+    <table className="table">
+      <TableHeader
+        columns={columns}
+        sortColumn={sortColumn}
+        onSort={onSort}
+      ></TableHeader>
+      <TableBody columns={columns} data={data}></TableBody>
+    </table>
+  );
+};
+
+export default Table;
--- vidlyu/src/components/moviesTable.jsx
+++ vidlyu/src/components/moviesTable.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import Like from "./common/like";
-import TableHeader from "./common/tableHeader";
-import TableBody from "./common/tableBody";
+import Table from "./common/table";
 
 class MoviesTable extends Component {
   columns = [
@@ -34,14 +33,12 @@ class MoviesTable extends Component {
   render() {
     const { movies, onSort, sortColumn } = this.props;
     return (
-      <table className="table">
-        <TableHeader
-          columns={this.columns}
-          sortColumn={sortColumn}
-          onSort={onSort}
-        ></TableHeader>
-        <TableBody columns={this.columns} data={movies}></TableBody>
-      </table>
+      <Table
+        columns={this.columns}
+        data={movies}
+        sortColumn={sortColumn}
+        onSort={sortColumn}
+      ></Table>
     );
   }
 }
