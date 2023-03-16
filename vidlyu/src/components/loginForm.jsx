d2cc70398da6f52032651fe2fad489e2edb03356
zalogarciam <gegarciam95@gmail.com>
2023-03-16 16:55:43 -0500

    Basic validation for login

--- vidlyu/src/components/loginForm.jsx
+++ vidlyu/src/components/loginForm.jsx
@@ -8,17 +8,34 @@ class LoginForm extends Component {
 
   state = {
     account: { username: "", password: "" },
+    errors: {},
   };
 
   //   componentDidMount() {
   //     this.username.current.focus();
   //   }
 
+  validate = () => {
+    const errors = {};
+
+    const { account } = this.state;
+    if (account.username.trim() === "")
+      errors.username = "Username is required";
+    if (account.password.trim() === "")
+      errors.password = "Password is required";
+    return Object.keys(errors).length === 0 ? null : errors;
+  };
+
   handleSubmit = (e) => {
     e.preventDefault();
+
+    const errors = this.validate();
+    console.log(errors);
+    this.setState({ errors });
+    if (errors) return;
     // Call the server
-    const username = this.username.current?.value;
-    console.log("Submitted:" + username);
+
+    console.log("Submitted");
   };
 
   handleChange = ({ target: input }) => {
